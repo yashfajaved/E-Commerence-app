12c64a3cd2c160fba163cccfde6d5385e587fb82
yashfajaved <javedyashfa67@gmail.com>
2026-04-20 00:45:36 -0700

    Delete Task3/App.js

--- Task3/App.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import React from 'react';
-import Task1Skin_care from './Task1Skin_care';
-
-function App() {
-  return <Task1Skin_care />;
-}
-
-registerRootComponent(App);
