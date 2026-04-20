7c3217655742a15267f8f46d108676fdd5813261
yashfajaved <javedyashfa67@gmail.com>
2026-04-20 00:45:25 -0700

    Delete Task3/metro.config.js

--- Task3/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Force Metro to only use the node_modules in Task1, not the parent directory
-config.resolver.nodeModulesPaths = [__dirname + '/node_modules'];
-config.watchFolders = [__dirname];
-
-module.exports = config;
